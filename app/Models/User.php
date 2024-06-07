bb1048f52e462f08aab48fc6450a1114f7103794
RaoUmair98 <rao.kami98@gmail.com>
2024-06-07 05:24:42 -0700

    reset password

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Mail\SurvayInvitationMail;
+use App\Mail\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -64,12 +64,13 @@ class User extends Authenticatable
 
     public function sendPasswordResetNotification($token)
     {
-        // $this->notify(new CustomResetPassword($token));
-        $resetToken = $token;
-        $resetLink = URL::to('reset-password') . '/' . $resetToken . '?email=' . $this->email;
-
-        // Mail::to($this)->send(new SurvayInvitationMail($resetLink));
-    }
+   // $this->notify(new CustomResetPassword($token));
+   $resetToken = $token;
+   $user = User::where('email', $this->email)->first();
+   $resetLink = URL::to('reset-password') . '/' . $resetToken . '?email=' . $this->email;
+//	dd($resetLink);
+    Mail::to($this)->send(new ResetPassword($resetLink,$user));
+ }
 
     public function subordinates()
     {
